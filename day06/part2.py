e450626ec6b4963b82ef4ef1341fc8a4523a01bb
Christoph Budziszewski <christoph@budziszewski.de>
2023-12-07 20:12:24 +0100

    day06 part2 success

---
+++ day06/part2.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import re
+
+from functools import reduce
+import operator
+
+
+times = list()
+distances = list()
+with open('input') as f:
+    for line in f:
+        if line.startswith('Time'):
+            times.append( int(''.join(re.findall(r'\d+',line))))
+        if line.startswith('Dist'):
+            distances.append( int(''.join(re.findall(r'\d+',line))))
+
+print(f"times: {times}")
+print(f"dist: {distances}")
+
+
+def travel_distance(charge_time,race_duration):
+    speed = charge_time
+    travel_time = race_duration - charge_time
+    return travel_time * speed
+
+winning_charge_times = list()
+
+for race, raceduration in enumerate(times):
+    winning_charge_times.append(sum([ travel_distance(ct,raceduration)>distances[race] for ct in range(0,raceduration)]))
+
+print(reduce(operator.mul,winning_charge_times,1))
+
