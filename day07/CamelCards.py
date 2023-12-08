946728f206ad65dbd62ff333c0daa07a4f14ea16
Christoph Budziszewski <christoph@budziszewski.de>
2023-12-08 01:17:18 +0100

    day07 part2 success

--- day07/CamelCards.py
+++ day07/CamelCards.py
@@ -9,15 +9,22 @@ class HandType(Enum):
     FOUROFAKIND = 5
     FIVEOFAKIND = 6
 
-CARD_VALUE=['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
-CARD_VALUE.reverse()
-
 class Hand:
-    def __init__(self, cards, bid = 0):
+    def __init__(self, cards, bid = 0, joker = False):
+        self._CARD_VALUE=['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
+        if joker:
+            self._CARD_VALUE=['A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J']
+        self._CARD_VALUE.reverse()
         self.cards = cards
         self.bid = bid
+        self.joker = joker
 
     def handtype(self):
+        cardcount = sorted([self.cards.count(v) for v in set(self.cards)])
+        if self.joker and self.cards.count('J') < 5:
+            cardcount = sorted([self.cards.count(v) for v in set(self.cards.replace('J',''))])
+            cardcount[-1] += self.cards.count('J') 
+
         mapping = dict()
         mapping['11111'] = HandType.HIGHCARD
         mapping['1112']  = HandType.ONEPAIR
@@ -26,10 +33,10 @@ class Hand:
         mapping['23']    = HandType.FULLHOUSE
         mapping['14']    = HandType.FOUROFAKIND
         mapping['5']     = HandType.FIVEOFAKIND
-        return mapping[''.join(sorted([str(self.cards.count(v)) for v in set(self.cards)]))]
+        return mapping[''.join(map(str,cardcount))]
 
     def as_number(self):
-        return "" + f"{self.handtype().value}" + ''.join([ f"{CARD_VALUE.index(x):02d}" for x in self.cards])
+        return "" + f"{self.handtype().value}" + ''.join([ f"{self._CARD_VALUE.index(x):02d}" for x in self.cards])
 
     def __str__(self):
         return f"Hand({self.cards}, {self.bid})"
@@ -43,10 +50,8 @@ class Hand:
         if self.handtype().value > other.handtype().value:
             return -1
         for c1, c2 in zip(self.cards, other.cards):
-            if CARD_VALUE.index(c1) < CARD_VALUE.index(c2):
+            if self._CARD_VALUE.index(c1) < other._CARD_VALUE.index(c2):
                 return 1
-            if CARD_VALUE.index(c1) > CARD_VALUE.index(c2):
+            if self._CARD_VALUE.index(c1) > other._CARD_VALUE.index(c2):
                 return -1
         return 0
-
-
